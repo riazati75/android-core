c676502f922352bcce46f54aeb83582ac01dbb34
Mohammad Ali Riazati <mehdi.pheramon@gmail.com>
2023-10-31 18:34:14 +0330

    update: CoreRecyclerViewAdapter.kt

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -103,7 +103,7 @@ publishing {
 
             groupId    = "ir.farsroidx"
             artifactId = "android-core"
-            version    = "1.3.19"
+            version    = "1.3.20"
 
             afterEvaluate {
                 from(
