a651ea3cf2f561d273835874b4b86eda7ce75ccb
Mohammad Ali Riazati <mehdi.pheramon@gmail.com>
2023-11-15 04:15:05 +0330

    update: Log.kt

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -103,7 +103,7 @@ publishing {
 
             groupId    = "ir.farsroidx"
             artifactId = "android-core"
-            version    = "1.3.20"
+            version    = "1.3.21"
 
             afterEvaluate {
                 from(
--- core/src/main/kotlin/ir/farsroidx/core/additives/Log.kt
+++ core/src/main/kotlin/ir/farsroidx/core/additives/Log.kt
@@ -8,42 +8,52 @@ import android.util.Log
 
 private const val logStrTag = "CentralCore"
 
-fun vLog(vararg logs: Any?) {
-    logs.forEach { log ->
-        if ( log != null ) {
-            Log.v(logStrTag, log.toString())
+fun vLog(vararg logs: Any?, isActive: Boolean = true) {
+    if (isActive) {
+        logs.forEach { log ->
+            if ( log != null ) {
+                Log.v(logStrTag, log.toString())
+            }
         }
     }
 }
 
-fun iLog(vararg logs: Any?) {
-    logs.forEach { log ->
-        if ( log != null ) {
-            Log.i(logStrTag, log.toString())
+fun iLog(vararg logs: Any?, isActive: Boolean = true) {
+    if (isActive) {
+        logs.forEach { log ->
+            if ( log != null ) {
+                Log.i(logStrTag, log.toString())
+            }
         }
     }
 }
 
-fun dLog(vararg logs: Any?) {
-    logs.forEach { log ->
-        if ( log != null ) {
-            Log.d(logStrTag, log.toString())
+fun dLog(vararg logs: Any?, isActive: Boolean = true) {
+    if (isActive) {
+        logs.forEach { log ->
+            if ( log != null ) {
+                Log.d(logStrTag, log.toString())
+            }
         }
     }
 }
 
-fun wLog(vararg logs: Any?) {
-    logs.forEach { log ->
-        if ( log != null ) {
-            Log.w(logStrTag, log.toString())
+fun wLog(vararg logs: Any?, isActive: Boolean = true) {
+    if (isActive) {
+        logs.forEach { log ->
+            if ( log != null ) {
+                Log.w(logStrTag, log.toString())
+            }
         }
     }
 }
 
-fun eLog(vararg logs: Any?) {
-    logs.forEach { log ->
-        if ( log != null ) {
-            Log.e(logStrTag, log.toString())
+fun eLog(vararg logs: Any?, isActive: Boolean = true) {
+    if (isActive) {
+        logs.forEach { log ->
+            if ( log != null ) {
+                Log.e(logStrTag, log.toString())
+            }
         }
     }
 }
